76275d62b08415394da74ce4b589ee0fe3104641
BlackdaveLZ <deivyloor1999@gmail.com>
2024-03-25 12:41:18 -0500

    deivy Loor 22/03/2024

--- app/Http/Controllers/GeneraOrdenesController.php
+++ app/Http/Controllers/GeneraOrdenesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use  App\Models\GeneraOrdenes;
 use DB;
-
+use Auth;
 
 class GeneraOrdenesController extends Controller
 {
@@ -92,6 +92,25 @@ class GeneraOrdenesController extends Controller
 
 
         ];
+              return $meses;
+    }
+       
+
+    public function mesesLetra($nmes){
+        $result="";
+       $nmes==1?$result="E":"";
+       $nmes==2?$result="F":"";
+       $nmes==3?$result="M":"";
+       $nmes==4?$result="A":"";
+       $nmes==5?$result="MY":"";
+       $nmes==6?$result="J":"";
+       $nmes==7?$result="JL":"";
+       $nmes==8?$result="AG":"";
+       $nmes==9?$result="O":"";
+       $nmes==10?$result="S":"";
+       $nmes==11?$result="N":"";
+       $nmes==12?$result="D":"";
+       
     }
 
     public function generarOrdenes(Request $rq){
@@ -103,26 +122,30 @@ class GeneraOrdenesController extends Controller
         $mes=$datos['mes'];//mes
 
         // dd($datos);
-        $estudiantes=DB::select("SELECT * FROM matriculas m 
+        $estudiantes=DB::select("SELECT * , m.id as mat_id FROM matriculas m 
                     JOIN estudiantes e ON m.est_id=e.id
                     WHERE m.anl_id=$anl_id 
                     AND jor_id=$jor_id
                     AND m.mat_estado=1");
                     // dd($estudiantes);
-                   
+
+
+                    $valor_pagar=75;
+         
                     foreach($estudiantes as $e)
                     {
-                   $input[ 'mat_id']=; 
-                   $input['codigo']=;
-                   $input['fecha_registro']=;
-                   $input['valor_pagar']=;
-                   $input['fecha_pago']=;
-                   $input['valor_pagado']=;
-                   $input['estado']=;
-                   $input['mes']=;
-                   $input['responsable']=;
-                   $input['secuencial']=;
-                   $input['documento']=;  
+                        
+                   $input[ 'mat_id']=$e->mat_id;            //id de la matricula
+                   $input['codigo']=;             // codigo ejemplo :  GMG
+                   $input['fecha_registro']=date('Y-m-d');
+                   $input['valor_pagar']=$valor_pagar;
+                   $input['fecha_pago']=null;
+                   $input['valor_pagado']=0;     
+                   $input['estado']=0;         //// pendiente -> 0   ------ pagado -> 1
+                   $input['mes']=$mes;
+                   $input['responsable']=Auth::user()->usu_nombres;
+                   $input['secuencial']=;  // secuencial de la orden
+                   $input['documento']=null;    
                     }
        
      
